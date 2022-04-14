3f0f248c337fe757ba2667ccbc6f8e92a7072b37
dmzazz <99010567+dmzazz@users.noreply.github.com>
2022-04-14 21:00:43 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,197 @@
+<?php include 'header.php'; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>UndanganKita</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- CSS Me -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Fontawesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    
+</head>
+<body>
+
+<!-- Jumbotron -->
+
+<div class="jumbotron shadow-sm">
+  <div class="container-fluid" style="color: #137e6d;">
+    <p class="h3 col-5">Buat undangan pernikahan lebih praktis dengan undangan digital</p>
+		<p class="discount"><h2>DISCOUNT</h2></p>
+	    	<p class="angka-discount"><b>50%</b></p>
+    	<p>Syarat & ketentuan berlaku*</p>
+    <p><a href=""><button class="klik-disini text-light">KLIK DISINI</button></a></p>
+  </div>
+</div>
+
+<!-- Design -->
+
+<div class="design">
+	<div class="container-fluid">
+		<p class="h4">Pilihan tema dan design bervariasi</p>
+			<p>Buat undangan digital sesuai keinginanmu</p>
+
+				<p class="design-lainnya text-end"><a href=""><button class="design-lainnya-1 text-light">DESIGN LAINNYA</button></a></p>
+
+					<div class="cd row row-cols-1 row-cols-md-4 g-4 text-center">
+					  <div class="col">
+					    <div class="card h-100">
+					      <img src="..." class="card-img-top" alt="...">
+					      <div class="card-body">
+					        <p class="card-text"><a href="#" class="btn">Tema 1</a></p>
+					      </div>
+					    </div>
+					  </div>
+					  <div class="col">
+					    <div class="card h-100">
+					      <img src="..." class="card-img-top" alt="...">
+					      <div class="card-body">
+					        <p class="card-text"><a href="#" class="btn">Tema 2</a></p>
+					      </div>
+					    </div>
+					  </div>
+					  <div class="col">
+					    <div class="card h-100">
+					      <img src="..." class="card-img-top" alt="...">
+					      <div class="card-body">
+					        <p class="card-text"><a href="#" class="btn">Tema 3</a></p>
+					      </div>
+					    </div>
+					  </div>
+					  <div class="col">
+					    <div class="card h-100">
+					      <img src="..." class="card-img-top" alt="...">
+					      <div class="card-body">
+					        <p class="card-text"><a href="#" class="btn">Tema 4</a></p>
+					      </div>
+					    </div>
+					  </div>
+					</div>
+	</div>
+</div>
+
+<!-- Fitur -->
+
+<div class="fitur">
+	<div class="container-fluid">
+		<p class="h3 text-center" style="padding-top: 3rem;">Fitur Undangan Digital</p>
+		<div class="row row-cols-1 row-cols-md-3 g-4">
+			<div class="col">
+				<i class="far fa-camera"></i>
+					<h3>Galeri</h3>
+				<p>Pilih foto terbaik dan buat galeri sesuai dengan keinginanmu</p>
+			</div>
+
+			<div class="col">
+				<img src="">
+					<h3>Kalender</h3>
+				<p>Sesuaikan dengan tanggal pernikahanmu</p>
+			</div>
+
+			<div class="col">
+				<img src="">
+					<h3>Lokasi</h3>
+				<p>Terhubung dengan google maps</p>
+			</div>
+
+			<div class="col">
+				<img src="">
+					<h3>Kartu Ucapan</h3>
+				<p>Kartu ucapan online</p>
+			</div>
+
+			<div class="col">
+				<img src="">
+					<h3>Amplop Digital</h3>
+				<p>Pilih foto terbaik dan buat galeri sesuai dengan keinginanmu</p>
+			</div>
+
+			<div class="col">
+				<img src="">
+					<h3>Auto Play Music</h3>
+				<p>Pilih foto terbaik dan buat galeri sesuai dengan keinginanmu</p>
+			</div>
+		</div>
+
+	</div>
+</div>
+
+<!-- Harga -->
+
+<div class="harga">
+	<div class="container">
+		<p class="h3 text-center" style="padding-bottom: 2rem;">Pilih Tema Sesuai Budget Anda</p>
+		<div class="row row-cols-1 row-cols-md-3 g-4">
+		
+			<div class="col">
+				 <div class="card h-100">
+					<div class="card-body">
+					    <h3 class="text-center">Bronze</h3>
+							<h2 class="text-center">75k</h2>
+								<p>10 Photo</p>
+								<p>Custom Link</p>
+								<p>Gratis Tema</p>
+									<p class="card-text text-center"><a href="#" class="btn">Icon Keranjag</a></p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col">
+				 <div class="card h-100">
+					<div class="card-body">
+					    <h3 class="text-center">Silver</h3>
+							<h2 class="text-center">75k</h2>
+								<p>10 Photo</p>
+								<p>Custom Link</p>
+								<p>Gratis Tema</p>
+									<p class="card-text text-center"><a href="#" class="btn">Icon Keranjag</a></p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col">
+				 <div class="card h-100">
+					<div class="card-body">
+					    <h3 class="text-center">Gold</h3>
+							<h2 class="text-center">75k</h2>
+								<p>10 Photo</p>
+								<p>Custom Link</p>
+								<p>Gratis Tema</p>
+									<p class="card-text text-center"><a href="#" class="btn">Icon Keranjag</a></p>
+					</div>
+				</div>
+			</div>
+
+
+		</div>
+	</div>
+</div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+
+    <!-- Fontawesome JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"></script>
+
+    <!-- Kit Fontawesome JS -->
+    <script src="https://kit.fontawesome.com/f98c5ff00c.js" crossorigin="anonymous"></script>
+
+</body>
+</html>
+
+
+
+<?php include 'footer.php'; ?>
